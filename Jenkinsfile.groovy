7af65e5fadb341360a9e8ea78c4aa885596d8ec5
Sujata Kale <49482604+SujataKale97@users.noreply.github.com>
2019-05-09 10:22:58 +0530

    Create Jenkinsfile.groovy

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,18 @@
+node("master"){
+  stage ('Build') {
+ 
+    git credentialsId: 'ea4c3770-b2ed-4639-9ffc-cc3e586e454c', url: 'https://github.com/SujataKale97/boxfuse-sample-java-war-hello.git'
+    withMaven(
+        // Maven installation declared in the Jenkins "Global Tool Configuration"
+      maven: 'M3'){
+        
+    bat 'mvn clean package'
+    }
+  }
+  
+  
+  //stage ('deploy')
+ // {
+  //  bat '''copy C:\\Apps\\Jenkins\\jobs\\WebApp-Pipe\\workspace\\target\\*.war C:\\apache-tomcat-7.0.94\\webapps'''
+//}
+}
