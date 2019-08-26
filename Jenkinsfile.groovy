a2b3f93e88c20fe9bbb38904f538744898886e42
Sujata Kale <49482604+SujataKale97@users.noreply.github.com>
2019-08-26 09:03:19 -0700

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,18 +1,6 @@
 node("master"){
-  stage ('Build') {
- 
-    git credentialsId: 'ea4c3770-b2ed-4639-9ffc-cc3e586e454c', url: 'https://github.com/SujataKale97/boxfuse-sample-java-war-hello.git'
-    withMaven(
-        // Maven installation declared in the Jenkins "Global Tool Configuration"
-      maven: 'M3'){
-        
-    bat 'mvn clean package'
-    }
+  stage ('Build') { 
+    git credentialsId: 'ea4c3770-b2ed-4639-9ffc-cc3e586e454c'
+    docker build . -t boxfuse-app
   }
-  
-  
-  stage ('deploy')
-  {
-  bat '''copy C:\\Apps\\Jenkins\\jobs\\Ui-based-maven-app\\workspace\\target\\*.war C:\\apache-tomcat-7.0.94\\webapps'''
-}
 }
