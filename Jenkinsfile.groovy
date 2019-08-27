303baedced7d5be71fe9378d66096a5dac2a1254
Sujata Kale <49482604+SujataKale97@users.noreply.github.com>
2019-08-27 12:27:14 +0530

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -9,7 +9,7 @@ node("master"){
   {
     withCredentials([file(credentialsId: '09140495-c54b-4f11-8ce7-4fcc177b052d', variable: 'PUSHIMAGE')]) {
       bat '''
-      gcloud auth activate-service-account --key-file PUSHIMAGE
+      gcloud auth activate-service-account --key-file %PUSHIMAGE%
       gcloud config set project hello-world-241305
       docker-credential-gcr configure-docker
       docker tag boxfuse-app:latest gcr.io/hello-world-241305/Image-Repo:${BUILD_TIMESTAMP}
