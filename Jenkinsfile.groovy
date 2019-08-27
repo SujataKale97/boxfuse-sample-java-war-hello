f51ff870a17a9eb365d08291076d2439cdcdd634
Sujata Kale <49482604+SujataKale97@users.noreply.github.com>
2019-08-27 12:40:02 +0530

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -7,13 +7,9 @@ node("master"){
   }
   stage ('Push Docker Image')
   {
-    withCredentials([[$class: 'FileBinding',credentialsId: '09140495-c54b-4f11-8ce7-4fcc177b052d', variable: 'PUSHIMAGE']]) {
-      bat '''
-      gcloud auth activate-service-account --key-file=%PUSHIMAGE%
-      gcloud config set project hello-world-241305
-      docker-credential-gcr configure-docker
-      docker tag boxfuse-app:latest gcr.io/hello-world-241305/Image-Repo:${BUILD_TIMESTAMP}
-      docker push gcr.io/hello-world-241305/Image-Repo:${BUILD_TIMESTAMP}
+    bat '''
+      docker tag boxfuse-app:latest sujata1997/boxfuse-app:latest
+      docker push sujata1997/boxfuse-app:latest
       '''
     }
   }
