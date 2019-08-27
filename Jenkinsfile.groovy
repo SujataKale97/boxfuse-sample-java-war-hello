d4b2f6a746b6de9a223d96b18ec3c589ebe65dad
Sujata Kale <49482604+SujataKale97@users.noreply.github.com>
2019-08-27 10:48:04 +0530

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -3,8 +3,18 @@ node("master"){
     git credentialsId: 'ea4c3770-b2ed-4639-9ffc-cc3e586e454c', url: 'https://github.com/SujataKale97/boxfuse-sample-java-war-hello.git'
    bat '''
     docker build -t boxfuse-app .
-    docker tag boxfuse-app  sujata1997/boxfuse-app
-    docker push sujata1997/boxfuse-app
     '''
+  }
+  stage ('Push Docker Image')
+  {
+    withCredentials([file(credentialsId: '09140495-c54b-4f11-8ce7-4fcc177b052d', variable: 'PUSH-IMAGE')]) {
+      bat '''
+      gcloud auth activate-service-account --key-file $PUSH-IMAGE
+			gcloud config set project hello-world-241305
+      docker-credential-gcr configure-docker
+      docker tag boxfuse-app:latest gcr.io/hello-world-241305/Image-Repo:${BUILD_TIMESTAMP}
+      docker push gcr.io/hello-world-241305/Image-Repo:${BUILD_TIMESTAMP}
+      '''
+    }
   }
 }
