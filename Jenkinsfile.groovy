53e18544fa6a52e99a2c5d7d209dfc0cfbcdb60d
Sujata Kale <49482604+SujataKale97@users.noreply.github.com>
2019-08-28 13:04:00 +0530

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -18,9 +18,8 @@ node("master"){
   (for /f "tokens=1,* delims=]" %%A in ('"type busybox-deployment.yaml|find /n /v """') do (
 
       set line="%%B"
-    if %line% == "      - image: gcr.io/hello-world-241305/image-repo:image_tag" (
-     set line= "      - image: gcr.io/hello-world-241305/image-repo:%%BUILD_NUMBER%%" ) 
-)) >busybox-deployment1.yaml
+   echo %line%
+)) 
 '''
 /*    bat '''
     gcloud container clusters get-credentials standard-cluster-1 --zone us-central1-a --project hello-world-241305 '''
