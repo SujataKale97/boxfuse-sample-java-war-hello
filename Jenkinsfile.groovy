3b16424a9cd78dd8c179ac2850cde85ac93c371d
Sujata Kale <49482604+SujataKale97@users.noreply.github.com>
2019-08-28 12:50:14 +0530

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,10 +1,10 @@
 node("master"){
  stage ('Build') { 
     git credentialsId: 'ea4c3770-b2ed-4639-9ffc-cc3e586e454c', url: 'https://github.com/SujataKale97/boxfuse-sample-java-war-hello.git'
-   bat '''
-     // mvn clean package
-     // docker build -t boxfuse-app .
-    '''
+  /* bat '''
+     mvn clean package
+     docker build -t boxfuse-app .
+    '''*/
   }/*
  stage ('Push Docker Image')
   {
