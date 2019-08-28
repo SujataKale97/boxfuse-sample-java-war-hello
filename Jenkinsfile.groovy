aaa0f2a0c3a6f74a469880300896b7486e7f6879
Sujata Kale <49482604+SujataKale97@users.noreply.github.com>
2019-08-28 14:04:46 +0530

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -16,7 +16,7 @@ node("master"){
   {
   bat '''
   (for /f "tokens=1,* delims=]" %%A in ('"type busybox-deployment.yaml|find /n /v """') do (
-      echo %%B >busybox-deployment1.yaml
+      echo %%B >> busybox-deployment1.yaml
 )) 
 '''
 /*    bat '''
