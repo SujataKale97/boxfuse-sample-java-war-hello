0aa386c3c8fe56ad092e51f0305d4885d38cfa85
Sujata Kale <49482604+SujataKale97@users.noreply.github.com>
2019-08-28 14:57:47 +0530

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -3,12 +3,12 @@ node("master"){
     git credentialsId: 'ea4c3770-b2ed-4639-9ffc-cc3e586e454c', url: 'https://github.com/SujataKale97/boxfuse-sample-java-war-hello.git'
    bat '''
      mvn clean package
-     docker build -t boxfuse-app .
     '''
   }
  stage ('Push Docker Image')
   {
     bat '''
+     docker build -t boxfuse-app .
       docker tag boxfuse-app:latest gcr.io/hello-world-241305/image-repo:%BUILD_NUMBER%
       docker push gcr.io/hello-world-241305/image-repo:%BUILD_NUMBER%
       '''
