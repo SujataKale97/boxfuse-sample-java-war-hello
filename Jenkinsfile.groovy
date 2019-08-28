f25e0aa7546fce1cf1f5965472d84e7dc3d401ba
Sujata Kale <49482604+SujataKale97@users.noreply.github.com>
2019-08-28 12:55:58 +0530

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -18,8 +18,8 @@ node("master"){
   (for /f "tokens=1,* delims=]" %%A in ('"type busybox-deployment.yaml|find /n /v """') do (
 
       set "line=%%B"
-    if defined line == "image: gcr.io/hello-world-241305/image-repo:image_tag" 
-     set line= "image: gcr.io/hello-world-241305/image-repo:%%BUILD_NUMBER%%" 
+    if defined line == "image: gcr.io/hello-world-241305/image-repo:image_tag" (
+     set line= "image: gcr.io/hello-world-241305/image-repo:%%BUILD_NUMBER%%" ) ELSE echo.
 )) >busybox-deployment1.yaml
 '''
 /*    bat '''
