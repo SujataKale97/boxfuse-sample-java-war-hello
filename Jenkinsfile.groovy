ec196ddee687e1cbdb47982321e965698ae74a2b
Sujata Kale <49482604+SujataKale97@users.noreply.github.com>
2019-08-28 14:55:31 +0530

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -2,33 +2,34 @@ node("master"){
  stage ('Build') { 
     git credentialsId: 'ea4c3770-b2ed-4639-9ffc-cc3e586e454c', url: 'https://github.com/SujataKale97/boxfuse-sample-java-war-hello.git'
    bat '''
-     
+     mvn clean package
+     docker build -t boxfuse-app .
     '''
-  }/*
+  }
  stage ('Push Docker Image')
   {
     bat '''
       docker tag boxfuse-app:latest gcr.io/hello-world-241305/image-repo:%BUILD_NUMBER%
       docker push gcr.io/hello-world-241305/image-repo:%BUILD_NUMBER%
       '''
-  }*/
+  }
   stage ('Deployment')
   {
   bat '''
-  echo #Deployment file  > busybox-deployment1.yaml
-SETLOCAL ENABLEDELAYEDEXPANSION
-for /f "delims=" %%a in (busybox-deployment.yaml) do (
-    SET s=%%a
-    SET s=!s:image_tag=%BUILD_NUMBER%!
-    echo !s! >> busybox-deployment1.yaml
-)
-move /Y busybox-deployment1.yaml busybox-deployment.yaml
-'''
-/*    bat '''
+     echo #Deployment file  > busybox-deployment1.yaml
+     SETLOCAL ENABLEDELAYEDEXPANSION
+     for /f "delims=" %%a in (busybox-deployment.yaml) do (
+         SET s=%%a
+         SET s=!s:image_tag=%BUILD_NUMBER%!
+         echo !s! >> busybox-deployment1.yaml
+     )
+     move /Y busybox-deployment1.yaml busybox-deployment.yaml
+     '''
+   bat '''
     gcloud container clusters get-credentials standard-cluster-1 --zone us-central1-a --project hello-world-241305 '''
-    bat '''
+   bat '''
     kubectl delete deployment -l app=boxfuse
     kubectl create -f busybox-deployment.yaml
-    '''*/
+    '''
   }
 }
