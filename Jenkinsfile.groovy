c99238f59c4ea3975938597135627ee8c8f91958
Sujata Kale <49482604+SujataKale97@users.noreply.github.com>
2019-08-28 11:24:03 +0530

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -2,10 +2,10 @@ node("master"){
   stage ('Build') { 
     git credentialsId: 'ea4c3770-b2ed-4639-9ffc-cc3e586e454c', url: 'https://github.com/SujataKale97/boxfuse-sample-java-war-hello.git'
    bat '''
-    docker build -t boxfuse-app .
+      mvn clean package
     '''
   }
-  stage ('Push Docker Image')
+ /* stage ('Push Docker Image')
   {
     bat '''
       docker tag boxfuse-app:latest gcr.io/hello-world-241305/image-repo:boxfuse
@@ -20,5 +20,5 @@ node("master"){
     kubectl delete deployment -l app=boxfuse
     kubectl create -f busybox-deployment.yaml
     '''
-  }
+  }*/
 }
