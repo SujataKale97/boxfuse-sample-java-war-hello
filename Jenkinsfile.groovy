90b08485bb9dc43d850dd2d99d6b0efcad51333c
Sujata Kale <49482604+SujataKale97@users.noreply.github.com>
2019-09-18 10:12:10 +0530

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,39 +1,45 @@
 node("master"){
- stage ('Build') { 
+ stage ('Build') 
+ { 
     git credentialsId: 'ea4c3770-b2ed-4639-9ffc-cc3e586e454c', url: 'https://github.com/SujataKale97/boxfuse-sample-java-war-hello.git'
-   bat '''
-     mvn clean package
-    '''
-  }
- stage('Sonar Quality Gate'){
-    bat 'gradle build sonarqube'
-  }
+    bat 'mvn clean package '
+ }
+ stage('Sonar Quality Gate')
+ {
+   //Sonar Step
+   bat 'mvn sonar:sonar -Dsonar.projectName=Maven-WebApp -Dsonar.host.url=http://localhost:9000'
+ }
  stage ('Push Docker Image')
-  {
+ {
+    bat 'docker build -t boxfuse-app . '  //Build an image using Dockerfile
+  
+    //Push image to image repository
     bat '''
-     docker build -t boxfuse-app . '''
-   bat '''
       docker tag boxfuse-app:latest gcr.io/hello-world-241305/image-repo:%BUILD_NUMBER%
       docker push gcr.io/hello-world-241305/image-repo:%BUILD_NUMBER%
-      '''
-  }
-  stage ('Deployment')
-  {
-  bat '''
-     echo #Deployment file  > busybox-deployment1.yaml
-     SETLOCAL ENABLEDELAYEDEXPANSION
-     for /f "delims=" %%a in (busybox-deployment.yaml) do (
-         SET s=%%a
-         SET s=!s:image_tag=%BUILD_NUMBER%!
-         echo !s! >> busybox-deployment1.yaml
-     )
-     move /Y busybox-deployment1.yaml busybox-deployment.yaml
      '''
+ }
+ stage ('Deployment')
+ {
+  //Replacing docker latest image in deployment file
    bat '''
-    gcloud container clusters get-credentials standard-cluster-1 --zone us-central1-a --project hello-world-241305 '''
-   bat '''
-    kubectl delete deployment -l app=boxfuse
-    kubectl create -f busybox-deployment.yaml
+       echo #Deployment file  > busybox-deployment1.yaml
+       SETLOCAL ENABLEDELAYEDEXPANSION
+       for /f "delims=" %%a in (busybox-deployment.yaml) do (
+           SET s=%%a
+           SET s=!s:image_tag=%BUILD_NUMBER%!
+           echo !s! >> busybox-deployment1.yaml
+       )
+       move /Y busybox-deployment1.yaml busybox-deployment.yaml
+    '''
+  
+   //Starting kubernetes cluster
+    bat 'gcloud container clusters get-credentials standard-cluster-1 --zone us-central1-a --project hello-world-241305 '
+  
+    //Deploying the latest application
+    bat '''
+      kubectl delete deployment -l app=boxfuse
+      kubectl create -f busybox-deployment.yaml
     '''
   }
 }
